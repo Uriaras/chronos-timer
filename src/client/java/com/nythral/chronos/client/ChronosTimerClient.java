42f97dd05ada15c11a10a8606cfbbb817e12a369
Jakub Dmochowski <jakubson03@outlook.com>
2026-07-24 09:59:38 +0200

    Initial Chronos Timer project

---
+++ src/client/java/com/nythral/chronos/client/ChronosTimerClient.java
@@ -0,0 +1,10 @@
+package com.nythral.chronos.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChronosTimerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
